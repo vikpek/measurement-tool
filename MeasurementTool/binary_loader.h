6ebab4d256346e0e5d9647b00504c0c3eb2a6eb5
Viktor Pekar <vikpek@gmail.com>
2011-04-02 09:26:32 +0200

    first commit

---
+++ MeasurementTool/binary_loader.h
@@ -0,0 +1,7 @@
+//
+//  BinaryLoader.h
+//  MeasurementTool
+//
+//  Created by Viktor Pekar on 30.03.11.
+//  Copyright 2011 SwArc. All rights reserved.
+//
---
+++ MeasurementTool/input_manager.cpp
@@ -0,0 +1,9 @@
+//
+//  InputManager.cpp
+//  MeasurementTool
+//
+//  Created by Viktor Pekar on 30.03.11.
+//  Copyright 2011 SwArc. All rights reserved.
+//
+
+#include "InputManager.h"
